eeae37a0bea00152b0f65a77df0e29f7f7e1f603
vvtrinadh123 <trinadhvenkat.v@gmail.com>
2026-05-28 00:48:13 +0530

    api doc added

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ do
         --image-id ami-0220d79f3f480ecf5 \
         --instance-type t3.micro \
         --security-groups "roboshop-common" "roboshop-$instance" \
-	    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="roboshop-$instance"}]' \
+	    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
 	    --query 'Instances[0].InstanceId' \
         --output text
 

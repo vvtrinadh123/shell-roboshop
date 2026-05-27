8cc0256589c1d41a6b695dbf45b3e1bfd353d06c
vvtrinadh123 <trinadhvenkat.v@gmail.com>
2026-05-27 16:23:19 +0530

    api doc added

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -32,3 +32,6 @@ VALIDATE $? "Adding Mongo repo"
 
 dnf install mongodb-org -y &>> $LOGS_FILE
 VALIDATE $? "Installing MongoDB"
+
+systemctl enable --now mongod
+VALIDATE $? "Starting and enabling MongoDB"
